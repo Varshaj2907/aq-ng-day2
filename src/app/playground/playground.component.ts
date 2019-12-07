639427cd2a887eadd4c28ee0529edcd8f46faf64
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-07 10:51:00 +0530

    switch

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
-  cities: string[] = ['Bengaluru', 'Chennai', 'Mysuru'];
+  lang = 'HN';
 
   constructor() {}
 
