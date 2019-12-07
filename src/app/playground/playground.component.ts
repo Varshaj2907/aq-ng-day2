be1f1afb9731fa8d3ef19e090348231bc0d52139
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-07 10:44:30 +0530

    array type-safing

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
-  cities = ['Bengaluru', 'Chennai', 'Mysuru'];
+  cities: string[] = ['Bengaluru', 'Chennai', 'Mysuru'];
 
   constructor() {}
 
