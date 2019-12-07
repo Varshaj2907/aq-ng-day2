4d542ac6bd0e7433e049a49c9f869698a73d27b0
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-07 16:58:56 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterContentChecked
+} from '@angular/core';
 import { DataService } from '../_services/data.service';
 
 @Component({
@@ -6,8 +17,47 @@ import { DataService } from '../_services/data.service';
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent implements OnInit {
-  constructor() {}
+export class PlaygroundComponent
+  implements
+    OnInit,
+    AfterViewInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterContentChecked {
+  @Input() field;
+  @Input() field2;
+  constructor() {
+    console.log('constructor');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  handler() {}
 }
